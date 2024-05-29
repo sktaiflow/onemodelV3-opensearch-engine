995071bb7c49463c1dc9f5dae04c0bf17f8ecb65
jaehwan <smart0924@gmail.com>
2024-05-29 16:53:55 +0900

    fix error message

--- dags/onemodelV3/error_code/internal_code.py
+++ dags/onemodelV3/error_code/internal_code.py
@@ -27,20 +27,27 @@ class InternalCodes(Enum):
     UPDATE_DOCUMENT_ERROR = "1004"
     DELETE_DOCUMENT_ERROR = "1005"
     SEARCH_ERROR = "1006"
+    CREATE_CLIENT_ERROR = "1007"
 
     # Success Code
     SUCCESS = auto()
 
+    # Warning Code
+    INDEX_EXIST = '900'
+    INDEX_CHECK = '901'
+    
     @staticmethod
     def get_message(code, e=''):
         messages = {
-            InternalCodes.PYDANTIC_VALIDATION_ERROR: f"[Pydantic] Pydantic validation check error, detail_message:{e}",
-            InternalCodes.CREATE_INDEX_ERROR: f"[Opensearch] Create index error, detail_message:{e}",
-            InternalCodes.DELETE_INDEX_ERROR: f"[Opensearch] Delete index error, detail_message:{e}",
-            InternalCodes.INDEXING_ERROR: f"[Opensearch] Indexing error, detail_message:{e}",
-            InternalCodes.UPDATE_DOCUMENT_ERROR: f"[Opensearch] Update document error, detail_message:{e}",
-            InternalCodes.DELETE_DOCUMENT_ERROR: f"[Opensearch] Delete document error, detail_message:{e}",
-            InternalCodes.SEARCH_ERROR: f"[Opensearch] Search error, detail_message:{e}",
+            InternalCodes.PYDANTIC_VALIDATION_ERROR: f"[ERROR] Pydantic validation check error, detail_message:{e}",
+            InternalCodes.CREATE_INDEX_ERROR: f"[ERROR] Create index error, detail_message:{e}",
+            InternalCodes.DELETE_INDEX_ERROR: f"[ERROR] Delete index error, detail_message:{e}",
+            InternalCodes.INDEXING_ERROR: f"[ERROR] Indexing error, detail_message:{e}",
+            InternalCodes.UPDATE_DOCUMENT_ERROR: f"[ERROR] Update document error, detail_message:{e}",
+            InternalCodes.DELETE_DOCUMENT_ERROR: f"[ERROR] Delete document error, detail_message:{e}",
+            InternalCodes.SEARCH_ERROR: f"[ERROR] Search error, detail_message:{e}",
+            InternalCodes.INDEX_EXIST: "[WARNING] INDEX EXISTS",
+            InternalCodes.INDEX_CHECK: "[ERROR] INDEX CHECK FAILED",
             InternalCodes.SUCCESS: "[Success]",
         }
         return messages.get(code, f"Undefined code, detail_message:{e}")
