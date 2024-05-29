b5152b17c46a74b03b03533592878d638a46efb9
jaehwan <smart0924@gmail.com>
2024-05-29 17:14:58 +0900

    func.py for opnsearch indexing engine

--- dags/onemodelV3/opensearch_engine/indexing_engine/func.py
+++ dags/onemodelV3/opensearch_engine/indexing_engine/func.py
@@ -2,13 +2,68 @@ from time import time
 import logging
 from opensearchpy import helpers, OpenSearch
 from datetime import datetime
-from dags.onemodelV3.common.my_logging import logger
+from dags.onemodelV3.logging import loguru_logger
 from opensearch_schema import ClientSetting
 import traceback
+from loguru import Logger
+from opensearch_schema import IndexingSchema
+from pydantic import BaseModel, ValidationError
+from error_code import InternalCodes
+from functools import wraps
+from typing import Callable, Any, Dict, Optional
 
 
-def create_client(client_setting:ClientSetting,  logger, **kwargs):
-    client = OpenSearch(
+def doc_validation_check(doc_body):
+    try:
+        IndexingSchema(**doc_body)
+        code = InternalCodes.SUCCESS
+    except ValidationError as e:
+        code = InternalCodes.PYDANTIC_VALIDATION_ERROR
+    finally:
+        return code
+
+def handle_operation(error_code: InternalCodes):
+    def decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                response = func(*args, **kwargs)
+                code = InternalCodes.SUCCESS
+                message = InternalCodes.get_message(code)
+            except Exception as e:
+                code = error_code
+                message = InternalCodes.get_message(code, e)
+                response = None
+            finally:
+                return {"response": response, "code": code, "message": message}
+        return wrapper
+    return decorator
+
+@handle_operation(InternalCodes.DELETE_DOCUMENT_ERROR)
+def delete_documents(client, index_name, doc_id, logger):
+    logger.info(f"Deleting document {doc_id} from index {index_name}")
+    return client.delete(index=index_name, id=doc_id)
+    
+
+@handle_operation(InternalCodes.UPDATE_DOCUMENT_ERROR)
+def update_documents(client, index_name, doc_id, doc_body, logger):
+    code = doc_validation_check(doc_body=doc_body)
+    message = InternalCodes.get_message(code=code)
+
+    if code != InternalCodes.SUCCESS:
+        logger.error(message)
+        return {"response": None, "code": code, "message": message}
+
+    update_body = {
+        "doc": doc_body,
+        "doc_as_upsert": True
+    }
+    logger.info(f"Updating document {doc_id} in index {index_name}")
+    return client.update(index=index_name, id=doc_id, body=update_body)
+
+@handle_operation(InternalCodes.CREATE_CLIENT_ERROR)
+def create_client(client_setting:ClientSetting,  logger:Logger, **kwargs):
+    return OpenSearch(
         hosts = [{"host": client_setting.host, "port": client_setting.port}],
         http_compress=client_setting.http_compress,
         use_ssl=client_setting.use_ssl,
@@ -17,55 +72,38 @@ def create_client(client_setting:ClientSetting,  logger, **kwargs):
         pool_maxsize=client_setting.pool_maxsize,
         http_auth=client_setting.http_auth
     )
-    logger.info(client)
-    return client
 
+@handle_operation(InternalCodes.CREATE_INDEX_ERROR)
 def create_index(client, index_name, index_body, logger):
     """ 신규 인덱스를 생성 """
-    try:
-        is_exists = index_check(client=client, index_name=index_name)
-        if not is_exists:
-            logger.info("[create_index] create new index")
-            client.indices.create(index=index_name, body=index_body)
-            return True
-        else:
-            logger.info(f"[index_check] index: {index_name} exists")
-            return False
-    except Exception as e:
-        logger.error(traceback.format_exc())
+    is_exists = index_check(client=client, index_name=index_name)
+    if not is_exists:
+        logger.info("[create_index] create new index")
+        return client.indices.create(index=index_name, body=index_body)
+    else:
+        logger.info(f"[index_check] index: {index_name} exists")
+        code = InternalCodes.INDEX_EXIST
+        message = InternalCodes.get_message(code)
+        return {"response": None, "code": code, "message": message}  
+        
 
-
+@handle_operation(InternalCodes.DELETE_INDEX_ERROR)
 def remove_index(client, index_name, logger):
     """인덱스를 삭제한다"""
+    logger.info(f"[remove_index] remove_index {index_name}")
+    return client.indices.delete(index=index_name)  
+        
+@handle_operation(InternalCodes.INDEX_CHECK)
+def index_check(client, index_name, logger:Logger):
+    return client.indices.exists(index_name)
 
-    if client.indices.exists(index_name):
-        client.indices.delete(index=index_name)
-        logger.info(f"[remove_index] remove_index {index_name}")
-
-def index_check(client, index_name):
-    if client.indices.exists(index_name):
-        logger.info(f"[index_check] index: {index_name} exists")
-        return True
-    else:
-        logger.info(f"[index_check] index: {index_name} not exists")
-        return False
-
-def indexing_data(client, generator):
-    """
-    신규 데이터를 지정한 index_name으로 집어 넣는다.
-    """
-    start_time = time.time()
-    resp = helpers.bulk(
-        client, 
-        generator, 
-        chunk_size=4000, 
-        request_timeout=300
+@handle_operation(InternalCodes.INDEXING_ERROR)
+def indexing_data(client, generator, logger:Logger, **kwargs):
+    """ data indexing """
+    return helpers.bulk(
+        client, generator, chunk_size=kwargs.get('chunk_size', 100), request_timeout=kwargs.get('chunk_size', 300)
     )
-    inference_time = time.time() - start_time
-    return resp, inference_time
 
-def update_data(opensearch, generator):
-    pass
 
 def analyze_query(client, index_name, query, analyzer:str=None):
     if analyzer is None:
