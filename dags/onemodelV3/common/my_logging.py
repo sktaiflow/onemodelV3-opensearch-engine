b08b6583b085cc14600a7f9b6daff68684d323f4
jaehwan <smart0924@gmail.com>
2024-05-29 12:50:12 +0900

    fix logger name

--- dags/onemodelV3/common/my_logging.py
+++ dags/onemodelV3/common/my_logging.py
@@ -1,9 +1,10 @@
+from __future__ import annotations
 import sys
-from loguru import logger
+from loguru import logger as loguru_logger
 from pathlib import Path
 
 # Remove the default loguru handlers
-logger.remove()
+loguru_logger.remove()
 
 # Set the log file path
 log_file_path = Path(__file__).parent / "logs" / "application.log"
@@ -12,7 +13,7 @@ log_file_path = Path(__file__).parent / "logs" / "application.log"
 log_file_path.parent.mkdir(exist_ok=True)
 
 # Add a file handler for writing logs to the file
-file_handler = logger.add(
+file_handler = loguru_logger.add(
     str(log_file_path),
     format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {file}:{line} | {message}",
     level="DEBUG",
@@ -22,14 +23,14 @@ file_handler = logger.add(
 )
 
 # Add a stream handler for writing logs to the console
-console_handler = logger.add(
+console_handler = loguru_logger.add(
     sys.stderr,
     format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{file}:{line}</cyan> - <level>{message}</level>",
     level="INFO",
     enqueue=True,
 )
 
-error_handler = logger.add(
+error_handler = loguru_logger.add(
     sys.stderr,
     format="<red>{time:YYYY-MM-DD HH:mm:ss}</red> | <red>{level: <8}</red> | <red>{file}:{line}</red> - <red>{message}</red>",
     level="ERROR",
@@ -39,4 +40,4 @@ error_handler = logger.add(
 # Optionally, you can add handlers for other logging levels or customize the logging format further
 
 # Export the logger instance for use in other modules
-logger = logger.bind(request_id=None)
+loguru_logger = loguru_logger.bind(request_id=None)
