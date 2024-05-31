5dba0c6cde83240a0882d94d9d2ba33e56a436a7
jaehwan <smart0924@gmail.com>
2024-05-31 19:06:32 +0900

    test

--- dags/main.py
+++ dags/main.py
@@ -1,3 +1,5 @@
+from skt.ye import slack_send
+
 def main(**kwargs):
     http_auth_id = kwargs.get('http_auth_id')
     http_auth_password = kwargs.get('http_auth_password')
@@ -10,6 +12,24 @@ def main(**kwargs):
     print(f"input_path: {input_path}")
     print(f"vpce_host: {vpce_host}")
     print(f"env: {env}")
+
+    template = f"""
+        http_auth_id Name: {http_auth_id}
+        http_auth_password: {http_auth_password}
+        input_path: {input_path}
+        vpce_host: {vpce_host}
+        env: {env}
+    """
+
+    slack_send(
+        text=template,
+        username="SKT",
+        channel="#prec_onemodel_alarming_stg",
+        icon_emoji=":large_blue_circle:",
+        blocks=None,
+        dataframe=False,
+        adot=True,
+    )
 
 if __name__ == "__main__":
     import sys
