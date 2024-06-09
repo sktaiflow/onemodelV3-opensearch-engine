71ff7822dbc82dc6bee9954251e400cd9a01155a
jaehwan <smart0924@gmail.com>
2024-06-09 12:11:56 +0900

    add decorator and change name

--- dags/onemodelV3/opensearch_engine/indexing_engine/func.py
+++ dags/onemodelV3/opensearch_engine/indexing_engine/func.py
@@ -11,7 +11,7 @@ from pydantic import BaseModel, ValidationError
 from functools import wraps
 from typing import Callable, Any, Dict, Optional
 
-def handle_operation(error_code: InternalCodes):
+def exception_handle_operation(error_code: InternalCodes):
     def decorator(func: Callable):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -28,13 +28,13 @@ def handle_operation(error_code: InternalCodes):
         return wrapper
     return decorator
 
-@handle_operation(InternalCodes.DELETE_DOCUMENT_ERROR)
+@exception_handle_operation(InternalCodes.DELETE_DOCUMENT_ERROR)
 def delete_documents(client, index_name, doc_id, logger):
     logger.info(f"Deleting document {doc_id} from index {index_name}")
     return client.delete(index=index_name, id=doc_id)
     
 
-@handle_operation(InternalCodes.UPDATE_DOCUMENT_ERROR)
+@exception_handle_operation(InternalCodes.UPDATE_DOCUMENT_ERROR)
 def update_documents(client, index_name, doc_id, doc_body, logger):
     update_body = {
         "doc": doc_body,
@@ -43,7 +43,7 @@ def update_documents(client, index_name, doc_id, doc_body, logger):
     logger.info(f"Updating document {doc_id} in index {index_name}")
     return client.update(index=index_name, id=doc_id, body=update_body)
 
-@handle_operation(InternalCodes.CREATE_CLIENT_ERROR)
+@exception_handle_operation(InternalCodes.CREATE_CLIENT_ERROR)
 def create_client(client_setting:ClientSetting,  logger, **kwargs):        
     return OpenSearch(
         hosts = [{"host": client_setting.host, "port": client_setting.port}],
@@ -55,23 +55,14 @@ def create_client(client_setting:ClientSetting,  logger, **kwargs):
         http_auth=client_setting.http_auth
     )
 
+@exception_handle_operation
 def create_index(client, index_name, index_body, logger):
     """ 신규 인덱스를 생성 """
-    is_exists = index_check(client=client, index_name=index_name)
-    if not is_exists:
-        logger.info("[create_index] create new index")
-        code = InternalCodes.SUCCESS
-        response = client.indices.create(index=index_name, body=index_body)
-        message = InternalCodes.get_message(code)
-        return {"response": response, "code": code.value, "message": message}  
-    else:
-        logger.info(f"[index_check] index: {index_name} exists")
-        code = InternalCodes.INDEX_EXIST
-        message = InternalCodes.get_message(code)
-        return {"response": None, "code": code.value, "message": message}  
+    response = client.indices.create(index=index_name, body=index_body)
+
         
 
-@handle_operation(InternalCodes.DELETE_INDEX_ERROR)
+@exception_handle_operation(InternalCodes.DELETE_INDEX_ERROR)
 def remove_index(client, index_name, logger):
     """인덱스를 삭제한다"""
     logger.info(f"[remove_index] remove_index {index_name}")
@@ -80,7 +71,7 @@ def remove_index(client, index_name, logger):
 def index_check(client, index_name):
     return client.indices.exists(index_name)
 
-@handle_operation(InternalCodes.INDEXING_ERROR)
+@exception_handle_operation(InternalCodes.INDEXING_ERROR)
 def indexing_data(client, generator, logger, **kwargs):
     """ data indexing """
     return helpers.bulk(
